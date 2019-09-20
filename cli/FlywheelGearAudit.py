8b649ff9ea550a696a4c8895c719f8d499c603e1
TinasheMTapera <teetaps@yahoo.com>
2019-09-20 10:10:36 -0400

    chmod

--- cli/FlywheelGearAudit.py
+++ cli/FlywheelGearAudit.py
@@ -3,97 +3,15 @@ import logging
 import warnings
 import argparse
 import pprint
+import os
+import sys
+import subprocess as sub
 import pandas as pd
 from tabulate import tabulate
 
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('Flywheel-Gear-Auditor')
-
-
-def gather_jobs(sessions_list, verbose):
-    '''
-    Creates a dataframe summarising the gear jobs that have run on the list of sessions
-    '''
-    df = pd.DataFrame()
-    for sess in sessions_list:
-
-        for al in sess.analyses:
-
-            basic_row = {
-                'subject': sess.subject.label,
-                'session': sess.label,
-                'gear_name': al.gear_info['name'],
-                'gear_version': al.gear_info['version'],
-                'run_label': al.label,
-                'run_datetime': al.job['created'],
-                'run_runtime_mins': al.job.profile['elapsed_time_ms'],
-                'run_status': al.job.state
-            }
-
-            final = pd.DataFrame(basic_row, index=[0])
-
-            if verbose:
-
-                config = al.job.config['config']
-                inputs = al.job.inputs
-                inputs = {k: v['name'] for k, v in inputs.items()}
-
-                config_cols = pd.DataFrame(list(config.items()), columns=['Config_Option', 'Config_Value'])
-                inputs_cols = pd.DataFrame(list(inputs.items()), columns=['Inputs_Option', 'Inputs_Value'])
-
-                final = pd.concat([final, inputs_cols, config_cols], axis=1)
-
-            df = pd.concat([df, final])
-
-    return(df)
-
-
-def gather_seqInfo(args, verbose):
-    '''
-    Runs fw-heudiconv-tabulate to attach sequence information to the gear jobs query
-    Inputs:
-        args --
-    '''
-
-
-    return None
-
-
-def gather_pipelines(sessions_list, verbose):
-
-    df = pd.DataFrame()
-    for sess in sessions_list:
-
-        for al in sess.analyses:
-
-            basic_row = {
-                'subject': sess.subject.label,
-                'session': sess.label,
-                'gear_name': al.gear_info['name'],
-                'gear_version': al.gear_info['version'],
-                'run_label': al.label,
-                'run_datetime': al.job['created'],
-                'run_runtime_mins': al.job.profile['elapsed_time_ms'],
-                'run_status': al.job.state
-            }
-
-            final = pd.DataFrame(basic_row, index=[0])
-
-            if verbose:
-
-                config = al.job.config['config']
-                inputs = al.job.inputs
-                inputs = {k: v['name'] for k, v in inputs.items()}
-
-                config_cols = pd.DataFrame(list(config.items()), columns=['Config_Option', 'Config_Value'])
-                inputs_cols = pd.DataFrame(list(inputs.items()), columns=['Inputs_Option', 'Inputs_Value'])
-
-                final = pd.concat([final, inputs_cols, config_cols], axis=1)
-
-            df = pd.concat([df, final])
-
-    return(df)
 
 
 def get_sessions(client, project_label, subject_labels=None, session_labels=None):
@@ -134,6 +52,97 @@ def get_sessions(client, project_label, subject_labels=None, session_labels=None
 
     sessions = [client.get(s.id) for s in sessions]
     return sessions
+
+
+def gather_jobs(sessions_list, verbose):
+    '''
+    Creates a dataframe summarising the gear jobs that have run on the list of sessions
+    '''
+    logger.info("Collecting gear run information...")
+    df = pd.DataFrame()
+    for sess in sessions_list:
+
+        for al in sess.analyses:
+
+            basic_row = {
+                'subject': sess.subject.label,
+                'session': sess.label,
+                'gear_name': al.gear_info['name'],
+                'gear_version': al.gear_info['version'],
+                'run_label': al.label,
+                'run_datetime': al.job['created'],
+                'run_runtime_mins': al.job.profile['elapsed_time_ms'],
+                'run_status': al.job.state
+            }
+
+            final = pd.DataFrame(basic_row, index=[0])
+
+            if verbose:
+
+                config = al.job.config['config']
+                inputs = al.job.inputs
+                inputs = {k: v['name'] for k, v in inputs.items()}
+
+                config_cols = pd.DataFrame(list(config.items()), columns=['Config_Option', 'Config_Value'])
+                inputs_cols = pd.DataFrame(list(inputs.items()), columns=['Inputs_Option', 'Inputs_Value'])
+
+                final = pd.concat([final, inputs_cols, config_cols], axis=1)
+
+            df = pd.concat([df, final])
+
+    return(df)
+
+
+def gather_seqInfo(args):
+    '''
+    Runs fw-heudiconv-tabulate to attach sequence information to the gear jobs query
+    Inputs:
+        args (from argparse)
+    '''
+    logger.info("Running fw-heudiconv-tabulate to collect sequence info...")
+    if args.path:
+        path = args.path
+    else:
+        path = './tmp'
+
+    command = ['mkdir', path]
+    p = sub.Popen(command, stdout=sub.PIPE, stdin=sub.PIPE, stderr=sub.PIPE, universal_newlines=True)
+
+    project_label = ' '.join(args.project)
+    command = ['fw-heudiconv-tabulate', '--project', project_label, '--path', path, '--no-unique']
+
+    if args.subject:
+        subjects = args.subject
+        subjects.insert(0, '--subject')
+        command.extend(subjects)
+
+    if args.session:
+
+        sessions = args.session
+        sessions.insert(0, '--session')
+        command.extend(sessions)
+
+    try:
+        p = sub.Popen(command, stdout=sub.PIPE, stdin=sub.PIPE, stderr=sub.PIPE, universal_newlines=True)
+        output, error = p.communicate()
+        if args.verbose:
+            logger.info(output)
+
+        outfile = os.listdir(path)
+        df = pd.read_csv(path + '/' + outfile[0], sep='\t')
+
+    except FileNotFoundError as e:
+
+        logger.error(e)
+        command = ['rm', '-r', path]
+        p = sub.Popen(command, stdout=sub.PIPE, stdin=sub.PIPE, stderr=sub.PIPE, universal_newlines=True)
+
+        return pd.DataFrame()
+
+    command = ['rm', '-r', path]
+    p = sub.Popen(command, stdout=sub.PIPE, stdin=sub.PIPE, stderr=sub.PIPE, universal_newlines=True)
+
+    return df
 
 
 def get_parser():
@@ -187,15 +196,17 @@ def get_parser():
 
     group2 = parser.add_mutually_exclusive_group(required=True)
     group2.add_argument(
-        "--by-run",
+        "--by-runs",
         help="Audit of every gear that has run in the query",
         default=None,
+        action='store_true',
         required=False
     )
     group2.add_argument(
-        "--by-sequence",
+        "--by-sequences",
         help="Audit of every gear that has run in the query incl. sequence info for filtering (not recommended with --verbose)",
         default=None,
+        action='store_true',
         required=False
     )
 
@@ -222,18 +233,35 @@ def main():
                     session_labels=args.session,
                     subject_labels=args.subject)
 
-    if args.by_run:
+    gear_df = gather_jobs(sessions, args.verbose)
 
-        gear_df = gather_jobs(sessions, args.verbose)
+    if gear_df.shape == (0, 0):
+        logger.info("No gears run for this query!")
+        sys.exit(1)
+    else:
 
-        if gear_df.shape == (0, 0):
-            logger.info("No gears run for this query!")
-        else:
+        if args.by_sequences:
 
-            if args.dry_run:
-                logger.info(tabulate(gear_df, headers='keys', tablefmt='psql'))
+            seq_df = gather_seqInfo(args)
+
+            if seq_df.shape == (0, 0):
+                logger.warning('fw-heudiconv-tabulate returned no seqInfo data!')
+                final_df = gear_df.copy()
+
             else:
-                gear_df.to_csv(path, sep=",", index=False)
 
+                logger.info('Merging sequence info and gear run data...')
+                seq_df.rename(columns={'patient_id': 'subject'}, inplace = True)
+                final_df = pd.merge(gear_df, seq_df, on='subject', how='outer')
+
+        if args.dry_run:
+            logger.info(tabulate(final_df, headers='keys', tablefmt='psql'))
+        else:
+            final_df.to_csv(path, sep=",", index=False)
+
+    logger.info("Done!")
+
+    sys.exit(0)
+    
 if __name__ == '__main__':
     main()
